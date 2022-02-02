4b99870b8e194c3bf6194b6c83b65fcefd956067
IvanKratenko <kratenko_ivan@mail.ru>
2022-02-03 00:53:29 +0700

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из четных чисел. Первоначальный массив можно ввести с клавиатуры, либо сгенерировать случайным образом. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    if (array.Length == 0)
+    {
+        Console.WriteLine("Массив пустой!");
+        return;
+    }
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+int EvenCounter(int[] array)
+{
+    int evenCounter = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0)
+        {
+            evenCounter++;
+        }
+    }
+    return evenCounter;
+}
+int[] GenerateNewArray(int[] array)
+{
+    int[] resultArray = new int[EvenCounter(array)];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+int[] workArray = new int[10];
+FillArray(workArray);
+PrintArray(workArray);
+int[] resultArray = GenerateNewArray(workArray);
+PrintArray(resultArray);
+
